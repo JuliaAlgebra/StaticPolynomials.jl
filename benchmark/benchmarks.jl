01a2ace81535126626e523f285fb73323d48be76
Sascha Timme <sascha.timme@gmail.com>
2018-03-07 10:21:32 +0100

    Add more benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,16 +5,27 @@ using StaticArrays
 using TestSystems
 
 function bevaluate(T, testsystem)
-    F = SP.system(TestSystems.equations(katsura5()))
+    F = SP.system(TestSystems.equations(testsystem))
     n = SP.nvariables(F)
     x = SVector{n, T}(rand(T, n))
     @benchmarkable SP.evaluate($F, $x)
 end
 
 const SUITE = BenchmarkGroup()
-
 SUITE["evaluation"] = BenchmarkGroup(["evaluation"])
-SUITE["evaluation"]["Float64"] = BenchmarkGroup()
-SUITE["evaluation"]["Float64"]["katsura5"] = bevaluate(Float64, katsura5())
-SUITE["evaluation"]["Float64"]["katsura6"] = bevaluate(Float64, katsura6())
-SUITE["evaluation"]["Float64"]["katsura10"] = bevaluate(Float64, katsura10())
+
+const all_systems = [
+    :katsura5, :katsura6, :katsura7, :katsura8, :katsura9, :katsura10,
+    :chandra4, :chandra5,
+    :cyclic5, :cyclic6, :cyclic7, :cyclic8,
+    :fourbar, :rps10]
+
+for T in [Float64, Complex128]
+    T_str = string(T)
+    SUITE["evaluation"][T_str] = BenchmarkGroup()
+    for system in all_systems
+        @eval begin
+            SUITE["evaluation"][$(T_str)][$(string(system))] = bevaluate($T, $(Expr(:call, system)))
+        end
+    end
+end
