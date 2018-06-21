1b685c32d81dfff9eb245bf1b9b14d0bf0b3f581
Sascha Timme <sascha.timme@gmail.com>
2018-06-21 11:16:48 +0200

    Add tests and remove trash from runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,13 +12,3 @@ include("codegen_tests.jl")
 include("basic_tests.jl")
 include("evaluation_tests.jl")
 include("gradient_tests.jl")
-
-A = [1 3 3; 0 2 3]
-
-isbits(SP.SExponents(A))
-
-@polyvar x y
-
-f = Polynomial(x^2+y^2+2y*x-3x^3*y)
-w = rand(2)
-SP.evaluate(f,w)
